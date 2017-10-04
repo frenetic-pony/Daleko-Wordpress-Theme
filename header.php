37bfcf21e2bc5c52492894275500db3489737991
nkryazhev <nkryazhev@gmail.com>
2017-10-04 18:23:34 +0400

    Fix header logo. Add monospaced font

--- header.php
+++ header.php
@@ -55,8 +55,8 @@
 
         <header id="masthead" class="site-header" role="banner">
             <div class="site-branding">
-                <a class="site-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><span style="font-size: 4rem;" class="icon-logo"></span>
-                </a>
+                <a class="site-logo" href="http://www.dalekoblog.com/" rel="home"><span style="font-size:4.5rem;" class="daleko-daleko-logo"></span>
+                  </a>
                 <!-- <?php bloginfo( 'name' ); ?> -->
                 <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
             </div><!-- .site-branding -->
